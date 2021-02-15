a13dc6660d155637c7980b53c856c1f88d36f744
Mudit Mahajan <muditmahajan21@gmail.com>
2021-02-15 20:04:01 +0530

    codechef feb

--- Codechef/2021/Codechef_FEB_LONG_2021/Prime_Game.cpp
+++ Codechef/2021/Codechef_FEB_LONG_2021/Prime_Game.cpp
@@ -1,23 +1,62 @@
 #include "bits/stdc++.h"
 #define ll long long int
 #define endl "\n"
+#define MAX 1000000
 using namespace std;
-
-void sieve_prime()
-{
-    
-}
 
 int main()
 {   
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     ll test;
+    
+    bool prime[MAX + 1];
+    memset(prime, true, sizeof(prime));
+    
+    for(ll p = 2; p * p <= MAX; p++)
+    {
+        if(prime[p])
+        {
+            for(ll i =p * 2; i <= MAX; i += p)
+            {
+                prime[i] = false;
+            }
+        }
+    }
+
+    vector<ll> primes;
+    primes.push_back(2);
+
+    for(ll i = 3; i < MAX; i += 2)
+    {
+        if(prime[i])
+        {
+            primes.push_back(i);
+        }
+    }        
+
     cin >> test;
     while(test--)
     {
         ll x, y, i, j, k, n, temp, count = 0, ans = 0, sum = 0;
         cin >> x >> y;
+
+        auto it = upper_bound(primes.begin(), primes.end(), x);
+
+        if(it != primes.begin())
+        {   
+            --it;
+            ans = it - primes.begin();
+        }
+
+        if(ans < y)
+        {
+            cout << "Chef" << endl;
+        }
+        else
+        {
+            cout << "Divyam" << endl;
+        }
     }
 
 return 0;
