a794923806250e9673f6bab763d15985246ce45e
muditmahajan21 <muditmahajan21@gmail.com>
2021-07-08 09:05:42 +0530

    codeforces

---
+++ Codeforces/Codefores_730_2021/A_Exciting_Bets.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+#define ll long long int
+#define endl "\n"
+using namespace std;
+
+int main()
+{   
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    ll test;
+    cin >> test;
+    while(test--)
+    {
+        ll i, j, k, n, temp, count = 0, ans = 0, sum = 0;
+        ll a, b;
+        cin >> a >> b;
+        
+        if(b > a)
+        {
+            swap(a, b);
+        }
+        ans = a - b;
+        if(ans == 0 || ans == 1)
+        {
+            count = 0;
+        }
+        else
+        {
+            count = min(a % ans, ans - a % ans);
+        }
+        cout << ans << " " << count << endl;
+    }
+
+return 0;
+}
